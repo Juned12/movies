923f98fdb6c1e9fcf22768a23bdb6c05faf3e5cf
Juned12 <mansurijuned111@gmail.com>
2024-01-02 20:56:19 +0530

    added inifinity scroll at top and bottom

--- src/screens/movies/index.js
+++ src/screens/movies/index.js
@@ -1,36 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./index.css"
 
 const BASE_IMG_URL = "https://image.tmdb.org/t/p/original"
 
 const Movies = ({
     movieYear,
-    movieList
+    movieList,
+    selectedGenreId,
 }) => {
-  return (
-    <div>
-        <div className='movie-year-text' id={movieYear}>
-            {movieYear}
-        </div>
-        <div className='movie-list-wrap'>
-            {
-                movieList.map((movData)=>{
-                    return (
-                        <div className='movie-card-wrap' key={movData?.id}>
-                            <img src={`${BASE_IMG_URL}${movData?.poster_path}`} alt="" className='movie-poster' loading='lazy'/>
-                            <div className='movie-name'>
-                                {movData?.original_title}
-                                <div>
-                                    {movData?.vote_average}
+
+    const getFilteredMovies = (genreId) => {
+        if(genreId) {
+            const filteredArr = movieList.filter((data)=>{
+                return data.genre_ids?.includes(genreId)
+            })
+            return filteredArr
+        } else {
+            return movieList
+        }
+    }
+    return (
+        <div className='first-node' id={movieYear}>
+            <div className='movie-year-text'>
+                {movieYear}
+            </div>
+            <div className='movie-list-wrap'>
+                {
+                    getFilteredMovies(selectedGenreId).map((movData) => {
+                        return (
+                            <div className='movie-card-wrap' key={movData?.id}>
+                                <img src={`${BASE_IMG_URL}${movData?.poster_path}`} alt="" className='movie-poster' loading='lazy' />
+                                <div className='movie-name'>
+                                    {movData?.original_title}
+                                    <div>
+                                        {movData?.vote_average}
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    )
-                })
-            }
+                        )
+                    })
+                }
+            </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default Movies;
--- src/screens/moviesList/index.js
+++ src/screens/moviesList/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import ReactDOMServer from 'react-dom/server';
 import { getMoviesData } from '../../services/movies';
 import GenreFilter from '../../components/genreFilter';
 import Movies from '../movies';
@@ -7,126 +8,173 @@ import "./index.css"
 const MoviesList = () => {
 
     const [currentYear, setCurrentYear] = useState(2012)
-    const [moviesData, setMoviesData] = useState([])
     const [allMoviesData, setAllMoviesData] = useState([])
+    const [moviesAfter2011, setMoviesAfter2011] = useState([])
     const [isFetching, setIsFetching] = useState(true)
-    const [selectedGenre, setSelectedGenre] = useState({id:0, name: "All"})
+    const [selectedGenre, setSelectedGenre] = useState({ id: 0, name: "All" })
 
     let lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
     useEffect(() => {
-        const handleScroll = (e) => {
-            const windowHeight = window.innerHeight;
-            const documentHeight = document.documentElement.scrollHeight;
-            const scrollTop = window.scrollY;
-            const upOffset = 150;
-            const downOffSet = 100
-            if (scrollTop < upOffset && !isFetching && scrollTop < lastScrollTop && !isFetching) {
-                if(currentYear-1 === 2011) {
-                    setCurrentYear(2010)
-                } else {
-                    setCurrentYear(currentYear-1)
-                }
-            }
-            if (scrollTop + windowHeight >= documentHeight - downOffSet && !isFetching && scrollTop > lastScrollTop) {
-                setCurrentYear(currentYear+1)
-            }
-            lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
-        };
-    
+
         window.addEventListener('scroll', handleScroll);
-    
+
         return () => {
-          window.removeEventListener('scroll', handleScroll);
+            window.removeEventListener('scroll', handleScroll);
         };
-      }, [isFetching, moviesData]);
+    }, [isFetching]);
 
-    useEffect(()=>{
-        getMoviesRecords(currentYear)
-    },[currentYear])
+    const handleScroll = (e) => {
+        const windowHeight = window.innerHeight;
+        const documentHeight = document.documentElement.scrollHeight;
+        const scrollTop = window.scrollY;
+        const upOffset = 150;
+        const downOffSet = 100
 
-    useEffect(()=>{
-        if(allMoviesData.length > 0) {
-            genreFilter(selectedGenre?.id)
+        if (scrollTop < upOffset && !isFetching && scrollTop < lastScrollTop && !isFetching) {
+            if (currentYear === 2012) {
+                setCurrentYear(currentYear - 2)
+            } else {
+                setCurrentYear(currentYear - 1)
+            }
         }
-    },[selectedGenre, allMoviesData])
+        if (scrollTop + windowHeight >= documentHeight - downOffSet && !isFetching && scrollTop > lastScrollTop) {
+            if (currentYear === 2011) {
+                setCurrentYear(currentYear + 2)
+            } else if(currentYear !== 2023){
+                setCurrentYear(currentYear + 1)
+            }
+        }
+        lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+    };
 
-    useEffect(()=>{
-        setTimeout(()=>{
+    useEffect(() => {
+        getMoviesRecords(currentYear)
+    }, [currentYear])
+
+
+    useEffect(() => {
+        setTimeout(() => {
             setCurrentYear(2011)
-        },500)
-    },[])
-    
+        }, 50)
+    }, [])
+
+
+    function fromHTML(html, trim = true) {
+        // Process the HTML string.
+        html = trim ? html : html.trim();
+        if (!html) return null;
+
+        // Then set up a new template element.
+        const template = document.createElement('template');
+        template.innerHTML = html;
+        const result = template.content.children;
+
+        // Then return either an HTMLElement or HTMLCollection,
+        // based on whether the input HTML had one or more roots.
+        if (result.length === 1) return result[0];
+        return result;
+    }
+
     const getMoviesRecords = async (year) => {
-        if(!moviesData[year]) {
             try {
                 setIsFetching(true)
                 const res = await getMoviesData(year)
                 const records = await res.json()
-                const movies = records.results.slice(0,20)
-                if(year >= 2012) {
-                    setAllMoviesData((moviesData) => [
+                const movies = records.results
+                setAllMoviesData((moviesData)=> [
+                    ...moviesData,
+                    { [year]: movies },
+                ])
+                if (year >= 2012) {
+                    setMoviesAfter2011((moviesData) => [
                         ...moviesData,
-                        {[year]: movies},
+                        { [year]: movies },
                     ])
                 } else {
-                    var old_height = document.documentElement.scrollHeight;
-                    var old_scroll = window.scrollY;
-                    setAllMoviesData((moviesData) => [
-                        {[year]: movies},
-                        ...moviesData
-                    ])
-                    setTimeout(()=>{
-                        window.scrollTo({
-                            top:(old_scroll + document.documentElement.scrollHeight - old_height) - 130
-                        })
-                    },20)
-                }           
+                    const firstMovie = document.querySelectorAll('.first-node')[0];
+                    if (firstMovie && currentYear) {
+                        const clonedMsg = <Movies
+                            movieYear={year}
+                            movieList={movies}
+                            selectedGenreId={selectedGenre?.id}
+
+                        />
+                        const newNode = fromHTML(ReactDOMServer.renderToString(clonedMsg))
+                        document.body.insertBefore(newNode, document.body.firstChild);
+                        document.documentElement.scrollTop = document.body.scrollTop = firstMovie.offsetTop - 130;
+                    }
+                }
             } catch (error) {
-                console.log("err",error)
+                const errorInfoDiv = document.createElement('div')
+                errorInfoDiv.className = 'error-wrap'
+                errorInfoDiv.innerText = "Something went wrong"
+                document.body.insertBefore(errorInfoDiv, document.body.firstChild);
+                console.log("err", error)
             } finally {
                 setIsFetching(false)
             }
-        }
     }
 
-    const genreFilter = (genreId) => {
-        if(genreId) {
-            const newArr = []
-            for (let index = 0; index < allMoviesData.length; index++) {
-                const element = allMoviesData[index];
-                const movYear = Object.keys(element)[0]
-                const filteredArr = element[movYear].filter((data)=>{
-                    return data.genre_ids.includes(genreId)
-                })
-                newArr.push({
-                    [movYear]:filteredArr 
-                })           
+    const getMoviesList = (year) => {
+        for (let index = 0; index < allMoviesData.length; index++) {
+            const movieYear = Object.keys(allMoviesData[index])[0]
+            if(movieYear == year) {
+                return allMoviesData[index][movieYear]
             }
-            setMoviesData(newArr)
-        } else {
-            setMoviesData(allMoviesData)
         }
+        return []
     }
-  return (
-    <>
-    <GenreFilter selectedGenre={selectedGenre} setSelectedGenre={(data)=>setSelectedGenre(data)}/>
-    <div className='movies-wrap'>
-      {
-        moviesData.map((movData, idx)=>{
-            const movYear = Object.keys(movData)[0]
-            return (
-                    <Movies
-                        movieYear={movYear}
-                        movieList={movData[movYear]}
-                        key={`${movYear}${idx}`}
+
+    useEffect(()=>{
+        if(selectedGenre?.id) {
+            updateDynamicallayCreatedNodes(selectedGenre)
+        }
+    },[selectedGenre])
+
+    const updateDynamicallayCreatedNodes = (selectedGenre) => {
+        const noOfYears = 2012 - currentYear
+        if(noOfYears > 0) {
+            for (let index = 1; index <= noOfYears; index++) {
+                const year = 2012 - index
+                const element = document.getElementById(year);
+                if(element) {
+                    const newMovie = <Movies
+                        movieYear={year}
+                        movieList={getMoviesList(year)}
+                        selectedGenreId={selectedGenre?.id}
                     />
-            )
-        })
-      }
-    </div>
-    </>
-  );
+                    const newNode = fromHTML(ReactDOMServer.renderToString(newMovie))
+                    const parentElement = element.parentElement;
+                    parentElement.replaceChild(newNode, element);
+                    element.remove()
+                }
+                
+            }
+        }
+        
+    }
+    
+    return (
+        <>
+            <GenreFilter selectedGenre={selectedGenre} setSelectedGenre={(data) => setSelectedGenre(data)} />
+            <div className='movies-wrap'>
+                {
+                    moviesAfter2011.map((movData, idx) => {
+                        const movYear = Object.keys(movData)[0]
+                        return (
+                            <Movies
+                                movieYear={movYear}
+                                movieList={movData[movYear]}
+                                key={`${movYear}${idx}`}
+                                selectedGenreId={selectedGenre?.id}
+                            />
+                        )
+                    })
+                }
+            </div>
+        </>
+    );
 }
 
 export default MoviesList;
